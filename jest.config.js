36af5ec8abcad6708ff79a8d35802c8f2b4dba2b
David Karolyi <davidkarolyi96@gmail.com>
2020-10-25 21:49:22 +0100

    feat: Initial project setup

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/lib/"],
+  coveragePathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/lib/"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageThreshold: {
+    global: { branches: 100, statements: 100, functions: 100, lines: 100 },
+  },
+};
